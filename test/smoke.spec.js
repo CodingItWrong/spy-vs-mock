eaf4d8de2fde3627c50a1e5aea75e874ed978068
Josh Justice <josh.justice@accesscfa.com>
2020-04-25 10:17:28 -0400

    Set up sinon test

--- test/smoke.spec.js
+++ test/smoke.spec.js
@@ -1,5 +1,20 @@
-describe('truth', () => {
-  it('is true', () => {
-    expect(true).to.eq(true);
+import sinon from 'sinon';
+import createWidget from '../src/createWidget';
+
+describe('createWidget', () => {
+  it('does the thing', () => {
+    const returnValue = 'returnValue';
+    const stubWidget = { name: 'stubWidget' };
+    const repo = {
+      save: () => {},
+    };
+
+    const mock = sinon.mock(repo);
+    mock.expects('save').once().withArgs(stubWidget).returns(returnValue);
+
+    const response = createWidget(stubWidget, repo);
+
+    expect(response).to.equal(returnValue);
+    mock.verify();
   });
 });
